06eae6becae58923b82797de70f9b009435f884b
Carybé <carybe.silva@usp.br>
2020-11-06 11:39:46 -0300

    Changes data collecting to use metric measurements provided by WU and adds datasets collected this way

--- scripts/wu_scrap.py
+++ scripts/wu_scrap.py
@@ -24,18 +24,18 @@ dataset = dict()
 for clas in classes:
 	dataset[clas] = []
 
-#url_base="https://www.wunderground.com/history/daily/br/s%C3%A3o-paulo/SBSP/date/"
-url_base="https://www.wunderground.com/history/daily/us/ny/new-york-city/KLGA/date/"
+#url_base="https://www.wunderground.com/history/daily/us/ny/new-york-city/KLGA/date/"
+url_base="https://www.wunderground.com/history/daily/br/s%C3%A3o-paulo/SBSP/date/"
 
-year = 2000
+year = 2015
 month = 1
 day = 1
 
 #records = 500
-records = 200000
+records = 2100
 
 #output='out.csv'
-output='wu_ny.csv'
+output='sp.csv'
 
 date = datetime.date(year, month, day)
 
@@ -45,6 +45,18 @@ options.add_argument("--headless")
 driver = Firefox(options=options, executable_path=gecko_path)
 driver.implicitly_wait(10)
 #wait = WebDriverWait(driver, 10)
+
+url = url_base + str(date)
+driver.get(url)
+while True:
+	driver.find_element_by_id('wuSettings').click()
+	try:
+		driver.find_element_by_xpath("//*[@title = 'Switch to Metric']").click()
+		break
+	except NoSuchElementException:
+		continue
+
+driver.refresh()
 
 with open(output, 'w') as file:
 
@@ -63,13 +75,18 @@ with open(output, 'w') as file:
 		month = date.month
 		day = date.day
 
-		while True:
-			try:
-				table = driver.find_elements_by_xpath("//*[@role = 'rowgroup']//*[@class = 'ng-star-inserted']")
-				break
-			except IndexError as e:
-				driver.refresh()
-				print("Retrying ", date)
+		# 3rd time is the charm
+		retries = 3
+		table = driver.find_elements_by_xpath("//*[@role = 'rowgroup']//*[@class = 'ng-star-inserted']")
+
+		# Because, if it isn't this date will be ignored
+		while not table and retries > 0:
+			retries -= 1
+			driver.refresh()
+			print("Retrying ", date)
+			table = driver.find_elements_by_xpath("//*[@role = 'rowgroup']//*[@class = 'ng-star-inserted']")
+		if retries == 0 and not table:
+			print("Skipping ", date)
 
 		for row_index in range(0, len(table), 17):
 
@@ -77,6 +94,7 @@ with open(output, 'w') as file:
 			cell_time = table[row_index + 0].text
 			time_epoch = -1
 			if cell_time:
+				# Conversion to Epoch from AM/PM
 				time_struct = datetime.datetime.strptime("%d-%.2d-%.2d %s" % (year, month, day, cell_time), "%Y-%m-%d %I:%M %p")
 				time_epoch = datetime.datetime.strftime(time_struct, "%s")
 			dataset['Time'].append(time_epoch)
@@ -85,20 +103,27 @@ with open(output, 'w') as file:
 			cell_temp = table[row_index + 1].text
 			temperature = -273
 			if cell_temp:
-				temperature = (float(cell_temp.split(' ')[0]) - 32.0) / 1.8
+				# Conversion to Celsius from Fahrenheit
+				# temperature = (float(cell_temp.split(' ')[0]) - 32.0) / 1.8
+				# No need to convert using metrics
+				temperature = float(cell_temp.split(' ')[0])
 			dataset['Temperature'].append(temperature)
 
 			# Dew Point (Celsius)
 			cell_dew = table[row_index + 3].text
 			dew_point = -273
 			if cell_dew:
-				dew_point = (float(cell_dew.split(' ')[0]) - 32.0) / 1.8
+				# Conversion to Celsius from Fahrenheit
+				# dew_point = (float(cell_dew.split(' ')[0]) - 32.0) / 1.8
+				# No need to convert using metrics
+				dew_point = float(cell_dew.split(' ')[0])
 			dataset['Dew Point'].append(dew_point)
 
 			# Humidity (percentage)
 			cell_hum = table[row_index + 5].text
 			humidity = -1
 			if cell_hum:
+				# Conversion to percentage [0,1]
 				humidity = float(cell_hum.split(' ')[0])/100
 			dataset['Humidity'].append(humidity)
 
@@ -113,28 +138,40 @@ with open(output, 'w') as file:
 			cell_wind_sp = table[row_index + 8].text
 			wind_speed = -1
 			if cell_wind_sp:
-				wind_speed = float(cell_wind_sp.split(' ')[0]) * 0.44704
+				# Conversion to m/s from mph
+				# wind_speed = float(cell_wind_sp.split(' ')[0]) * 0.44704
+				# Conversion to m/s from km/h
+				wind_speed = float(cell_wind_sp.split(' ')[0]) / 3.6
 			dataset['Wind Speed'].append(wind_speed)
 
 			# Wind Gust (m/s)
 			cell_wind_gust = table[row_index + 10].text
 			wind_gust = -1
 			if cell_wind_gust:
-				wind_gust = float(cell_wind_gust.split(' ')[0]) * 0.44704
+				# Conversion to m/s from mph
+				# wind_gust = float(cell_wind_gust.split(' ')[0]) * 0.44704
+				# Conversion to m/s from km/h
+				wind_gust = float(cell_wind_gust.split(' ')[0]) / 3.6
 			dataset['Wind Gust'].append(wind_gust)
 
 			# Atmospheric Pressure (hPa)
 			cell_pres = table[row_index + 12].text
 			pressure = -1
 			if cell_pres:
-				pressure = float(cell_pres.split(' ')[0]) * 33.863800000001
+				# Conversion to hPa from inHg
+				# pressure = float(cell_pres.split(' ')[0]) * 33.863800000001
+				# No need to convert using metrics
+				pressure = float(cell_pres.replace(',', '').split(' ')[0])
 			dataset['Pressure'].append(pressure)
 
 			# Preciptation (mm)
 			cell_prec = table[row_index + 14].text
 			precipitation = -1
 			if cell_prec:
+				# Conversion to mm from in
 				preciptation = float(cell_prec.split(' ')[0]) / 0.039370
+				# No need to convert using metrics
+				preciptation = float(cell_prec.split(' ')[0])
 			dataset['Precip.'].append(preciptation)
 
 			# Condition (categorical)
@@ -151,7 +188,4 @@ with open(output, 'w') as file:
 		date += datetime.timedelta(days=1)
 		file.flush()
 
-#print(dataset)
-
-#	for i in range(len(dataset['Condition'])):
 
